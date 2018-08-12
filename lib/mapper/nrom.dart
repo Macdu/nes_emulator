42301fae8fe61f56ddbf86b9d831b162e898ef54
Macdu <dufay.marc@gmail.com>
2018-08-12 11:53:23 +0200

    Fix NROM bug

--- lib/mapper/nrom.dart
+++ lib/mapper/nrom.dart
@@ -10,7 +10,7 @@ class NROMMapper extends Mapper {
     _cpu.memory.load_PGR_upper(_rom, _pgr_start + (1 << 14) * (_nb_pgr - 1));
 
     // not sure if I have to load this part
-    if (_nb_chr > 1) {
+    if (_nb_chr >= 1) {
       _cpu.ppu.memory.load_chr_rom(_rom, _chr_start);
     }
   }
